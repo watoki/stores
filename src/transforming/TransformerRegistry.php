bc3acb15858587d51d31fee0d1120c05b89efc8c
Nikolas Martens <Nikolas.M@rtens.org>
2016-03-09 16:56:03 +0600

    Fluent setters for TransformerRegistry

--- src/transforming/TransformerRegistry.php
+++ src/transforming/TransformerRegistry.php
@@ -6,14 +6,29 @@ class TransformerRegistry {
     /** @var Transformer[] */
     private $transformers = [];
 
+    /**
+     * @param Transformer $transformer
+     * @return $this
+     */
     public function add(Transformer $transformer) {
         $this->transformers[] = $transformer;
+        return $this;
     }
 
+    /**
+     * @param Transformer $transformer
+     * @return $this
+     */
     public function insert(Transformer $transformer) {
         array_unshift($this->transformers, $transformer);
+        return $this;
     }
 
+    /**
+     * @param mixed $value
+     * @return Transformer
+     * @throws \Exception
+     */
     public function toTransform($value) {
         foreach ($this->transformers as $transformer) {
             if ($transformer->canTransform($value)) {
@@ -23,6 +38,11 @@ class TransformerRegistry {
         throw new \Exception("No matching transformer found");
     }
 
+    /**
+     * @param mixed $transformed
+     * @return Transformer
+     * @throws \Exception
+     */
     public function toRevert($transformed) {
         foreach ($this->transformers as $transformer) {
             if ($transformer->hasTransformed($transformed)) {
